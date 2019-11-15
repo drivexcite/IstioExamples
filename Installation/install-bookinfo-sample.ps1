9dafe9b5e52787d5281d2cfc4909c985e134da02
Tona Diaz <tdiaz@healthwise.org>
2019-11-14 22:48:49 -0700

    Fixed username and password for Kiali.

--- Installation/install-bookinfo-sample.ps1
+++ Installation/install-bookinfo-sample.ps1
@@ -4,3 +4,7 @@ kubectl apply -f .\networking\bookinfo-gateway.yaml
 kubectl get pods --namespace istio-system
 # kiali-7dd44f7696-klf6j
 kubectl --namespace istio-system port-forward kiali-7dd44f7696-klf6j 20001:20001
+
+# It maybe necessary to restart kiali to allow for the secret to take effect
+kubectl scale deployment kiali --replicas=0 --namespace istio-system
+kubectl scale deployment kiali --replicas=1 --namespace istio-system
